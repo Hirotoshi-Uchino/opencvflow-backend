f0a5fc8349091b11655c250d722927f9bb1b9bbc
hirotoshi <u.hirotoshi@gmail.com>
2019-10-27 09:45:21 +0900

    [update] return error message

--- src/main.py
+++ src/main.py
@@ -6,20 +6,10 @@ from log import setup_logger
 from pipeline_parser import PipelineParser
 
 
-if __name__ == '__main__':
-    args = sys.argv
-    pipelineInfoString = args[1]
-
-    logger = setup_logger(__name__, './logs/process.log')
-    logger.info('pipeline start.')
-    logger.info('pipelineInfoString' + pipelineInfoString)
-
-    resultHeader = {'code': '000'}
-    pp = PipelineParser(pipelineInfoString)
-
-    img_ndarray = pp.get_image()
-
+def exec_pipeline(img_ndarray, pp):
     result = {'resultList': []}
+    existsError = False
+    trace = ''
     for p in pp.parse():
         process  = p['process']
 
@@ -29,8 +19,41 @@ if __name__ == '__main__':
             result['resultList'].append(this_result)
 
         except:
-            print(traceback.format_exc())
+            existsError = True
+            trace = traceback.format_exc()
+
+            break
 
     logger.info('pipeline end.')
+    if existsError:
+        resultHeader = {'code': '002'}
+        result = {'header': resultHeader}
+        result['errorMessage'] = trace
+    else:
+        resultHeader = {'code': '000'}
+        result['header'] = resultHeader
+
+    return result
+
+if __name__ == '__main__':
+    args = sys.argv
+    pipelineInfoString = args[1]
+
+    logger = setup_logger(__name__, './logs/process.log')
+
+    logger.info('pipeline start.')
+    logger.info('pipelineInfoString' + pipelineInfoString)
+
+    pp = PipelineParser(pipelineInfoString)
+
+    img_ndarray = pp.get_image()
+
+    if img_ndarray is None:
+        resultHeader = {'code': '001'}
+        result = {'header': resultHeader}
+        result = {'errorMessage': pp.imageFilePath + ': No such file.'}
+    else:
+        result = exec_pipeline(img_ndarray, pp)
+
 
     print(json.dumps(result))
